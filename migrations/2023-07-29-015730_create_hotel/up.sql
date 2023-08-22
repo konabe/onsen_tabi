0b77a794712af045511b3014e14461ed50a5b457
Ryota Konabe <rt.konabe@gmail.com>
2023-08-23 08:36:20 +0900

    fix migration

--- migrations/2023-07-29-015730_create_hotel/up.sql
+++ migrations/2023-07-29-015730_create_hotel/up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS hotel (
   id int unsigned NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
   has_washitsu tinyint(1) NOT NULL DEFAULT 0,
